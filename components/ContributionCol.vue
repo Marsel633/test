84b38ecd77f8d51b6deddf5413184221cfacb37e
Marsel633 <akparov1m@gmail.com>
2023-12-19 23:17:29 +0600

    replaced some funcs to store

--- components/ContributionCol.vue
+++ components/ContributionCol.vue
@@ -1,33 +1,18 @@
 <template>
   <div class="col">
     <ul class="months">
-      <li>Апр.</li>
-      <li>Май</li>
-      <li>Июнь</li>
-      <li>Июль</li>
-      <li>Авг.</li>
-      <li>Сент.</li>
-      <li>Окт.</li>
-      <li>Нояб.</li>
-      <li>Дек.</li>
-      <li>Янв.</li>
-      <li>Февр.</li>
-      <li>Март</li>
+      <li v-for="month in contributionStore.monthsArray" :key="month">
+        {{ month }}
+      </li>
     </ul>
     <ul class="weeks">
-      <li>
-        Пн
-      </li>
-      <li>
-        Ср
-      </li>
-      <li>
-        Пт
+      <li v-for="item in contributionStore.weeksArray" :key="item">
+        {{ item }}
       </li>
     </ul>
-    <div v-for="(value, key) in dates" :key="key">
-      <div @click="toggleModal(key)" class="contribution" :class="getColor(value)">
-        <div v-if="modalActive === key" class="modal">{{ value }} contributions<span>{{ key }}</span></div>
+    <div v-for="(value, key) in contributionStore.dates" :key="key">
+      <div @click="toggleModal(key)" class="contribution" :class="contributionStore.getColor(value)">
+        <div v-if="modalActive === key" class="modal"><span class="triangle"></span>{{ value }} contributions<span>{{ contributionStore.formatDateLong(key) }}</span></div>
       </div>
       <div @click="clickModalOutside" v-if="modalActive" class="overlay"></div>
     </div>
@@ -35,14 +20,10 @@
 </template>
 
 <script lang="ts" setup>
-const countributionStore = useContributionStore();
-const startDate: Date = new Date('2022-10-25');
-const endDate: Date = new Date('2023-10-25');
-const dates = ref<{ [key: string]: number }>({});
-const date: Date = new Date(startDate.getTime());
-const modalActive = ref<string | null>(null);
+const contributionStore = useContributionStore();
+const modalActive = ref<string | null | number>(null);
 
-const toggleModal = (key: string | any) => {
+const toggleModal = (key: string | number) => {
   if (modalActive.value === key) {
     modalActive.value = null;
   } else {
@@ -54,37 +35,14 @@ const clickModalOutside = () => {
   modalActive.value = null
 }
 
-const formatDate = (date: Date): string => {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;
-};
-
-date.setDate(date.getDate() + 1);
-while (date < endDate) {
-  const formattedDate = formatDate(date);
-  dates.value[formattedDate] = 0;
-  date.setDate(date.getDate() + 1);
-}
-
-const getColor = (number: number): string => {
-  if (number < 1) return 'defaultColor';
-  if (number > 1 && number <= 9) return 'low';
-  if (number > 9 && number <= 19) return 'medium';
-  if (number > 20 && number <= 29) return 'high';
-  if (number > 29) return 'highest';
-  return 'defaultColor';
-};
-
 onMounted(() => {
-  countributionStore.getData();
+  contributionStore.getData();
 });
 
 watchEffect(() => {
-  for (let key in dates.value) {
-    if (countributionStore.conData.hasOwnProperty(key) && dates.value.hasOwnProperty(key)) {
-      dates.value[key] = countributionStore.conData[key];
+  for (let key in contributionStore.dates) {
+    if (contributionStore.conData.hasOwnProperty(key) && contributionStore.dates.hasOwnProperty(key)) {
+      contributionStore.dates[key] = contributionStore.conData[key];
     }
   }
 });
@@ -102,7 +60,6 @@ watchEffect(() => {
   row-gap: 2px;
   position: relative;
 }
-
 .months {
   width: 100%;
   position: absolute;
@@ -110,7 +67,6 @@ watchEffect(() => {
   display: flex;
   gap: 4%;
 }
-
 .weeks {
   position: absolute;
   left: -70px;
@@ -123,7 +79,6 @@ li {
   list-style-type: none;
   color: #959494;
 }
-
 .contribution {
   width: 15px;
   height: 15px;
@@ -131,9 +86,8 @@ li {
   border: 1px solid transparent;
   position: relative;
 }
-
 .modal {
-  width: 145px;
+  width: 150px;
   height: 42px;
   background-color: #000;
   position: absolute;
@@ -147,11 +101,18 @@ li {
   flex-direction: column;
   z-index: 5;
 }
-
+.triangle {
+  width: 10px;
+  height: 10px;
+  background-color: #000;
+  clip-path: polygon(100% 0, 0 0, 50% 100%);
+  position: absolute;
+  bottom: -8px;
+}
 span {
   color: #7C7C7C;
+  font-size: 10px;
 }
-
 .overlay {
   position: fixed;
   top: 0;
@@ -161,27 +122,21 @@ span {
   background-color: transparent;
   z-index: 1;
 }
-
 .contribution:hover {
   border: 1px solid #000;
 }
-
 .defaultColor {
   background-color: #EDEDED;
 }
-
 .low {
   background-color: #ACD5F2;
 }
-
 .medium {
   background-color: #7FA8C9;
 }
-
 .high {
   background-color: #527BA0;
 }
-
 .highest {
   background-color: #254E77;
 }
--- stores/contributionStore.ts
+++ stores/contributionStore.ts
@@ -11,8 +11,70 @@ export const useContributionStore = defineStore("getData", () => {
       console.error(error);
     }
   };
+
+  const startDate: Date = new Date("2022-10-25");
+  const endDate: Date = new Date("2023-10-25");
+  const dates = ref<{ [key: string]: number }>({});
+  const date: Date = new Date(startDate.getTime());
+  const formatDate = (date: Date): string => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };
+
+  date.setDate(date.getDate() + 1);
+  while (date < endDate) {
+    const formattedDate = formatDate(date);
+    dates.value[formattedDate] = 0;
+    date.setDate(date.getDate() + 1);
+  }
+
+  const getColor = (number: number): string => {
+    if (number < 1) return "defaultColor";
+    if (number > 1 && number <= 9) return "low";
+    if (number > 9 && number <= 19) return "medium";
+    if (number > 20 && number <= 29) return "high";
+    if (number > 29) return "highest";
+    return "defaultColor";
+  };
+
+  const formatDateLong = (dateString: string | number): string => {
+    const date = new Date(dateString.toString());
+    
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    return new Intl.DateTimeFormat("ru-RU", options).format(date);
+  };
+
+
+  const monthsArray: string[] = [
+    "Апр.",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Авг.",
+    "Сент.",
+    "Окт.",
+    "Нояб.",
+    "Дек.",
+    "Янв.",
+    "Февр.",
+    "Март",
+  ];
+
+  const weeksArray: string[] = ["Пн", "Ср", "Пт"];
   return {
     getData,
-    conData
-  }
+    conData,
+    dates,
+    monthsArray,
+    weeksArray,
+    getColor,
+    formatDateLong
+  };
 });
